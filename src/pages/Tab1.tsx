b20a16078b281d5028aed42e34f5e8639f1ada51
BuzzDyne <buzzdyne@gmail.com>
2024-03-14 15:26:26 +0700

    Test commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +27,12 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonCard>
+          <IonCardHeader>Card Header</IonCardHeader>
+          <IonCardContent>
+            <IonInput type="email" label="Email" />
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
